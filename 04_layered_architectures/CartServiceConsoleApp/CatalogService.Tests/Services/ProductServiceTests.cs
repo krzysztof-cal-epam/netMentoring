5a1d414358990c9a56093ff6d8c4aea838c37015
ext-krysztof <ext-krysztof.cal@connect.onteam.blue>
2025-04-24 12:42:27 +0200

    Refactored tests

--- 04_layered_architectures/CartServiceConsoleApp/CatalogService.Tests/Services/ProductServiceTests.cs
+++ 04_layered_architectures/CartServiceConsoleApp/CatalogService.Tests/Services/ProductServiceTests.cs
@@ -8,29 +8,29 @@ namespace CatalogService.Tests.Services
 {
     public class ProductServiceTests
     {
+        private readonly Product _sampleProduct = new Product()
+        {
+            Id = 1,
+            Name = "Laptop ABC",
+            Description = "Description of the laptop ABC",
+            Price = 2999.99m,
+            Amount = 5,
+            CategoryId = 2,
+            Category = new Category
+            {
+                Id = 2,
+                Name = "Electronics"
+            }
+        };
+
         [Fact]
         public async Task GetById_ShouldReturnCorrectDto()
         {
             // Arrange
-            var sampleProduct = new Product
-            {
-                Id = 1,
-                Name = "Laptop ABC",
-                Description = "Description of the laptop ABC",
-                Price = 2999.99m,
-                Amount = 5,
-                CategoryId = 2,
-                Category = new Category
-                {
-                    Id = 2,
-                    Name = "Electronics"
-                }
-            };
-
             var mockProductRepository = new Mock<IProductRepository>();
             mockProductRepository
                 .Setup(repo => repo.GetByIdAsync(1))
-                .ReturnsAsync(sampleProduct);
+                .ReturnsAsync(_sampleProduct);
 
             var productService = new ProductService(mockProductRepository.Object, new Mock<IRepository<Category>>().Object);
 
@@ -40,37 +40,20 @@ namespace CatalogService.Tests.Services
             // Assert
             Assert.NotNull(result);
             Assert.IsType<ProductDto>(result);
-            Assert.Equal(sampleProduct.Id, result.Id);
-            Assert.Equal(sampleProduct.Name, result.Name);
-            Assert.Equal(sampleProduct.Price, result.Price);
-            Assert.Equal(sampleProduct.CategoryId, result.CategoryId);
+            Assert.Equal(_sampleProduct.Id, result.Id);
+            Assert.Equal(_sampleProduct.Name, result.Name);
+            Assert.Equal(_sampleProduct.Price, result.Price);
+            Assert.Equal(_sampleProduct.CategoryId, result.CategoryId);
         }
 
         [Fact]
         public async Task ProductService_GetProductsAsync_ShouldReturnData()
         {
             // Arrange
-            IEnumerable<Product> sampleProducts = new[] {
-                new Product
-                {
-                    Id = 1,
-                    Name = "Laptop ABC",
-                    Description = "Description of the laptop ABC",
-                    Price = 2999.99m,
-                    Amount = 5,
-                    CategoryId = 2,
-                    Category = new Category
-                    {
-                        Id = 2,
-                        Name = "Electronics"
-                    }
-                }
-            };
-
             var mockProductRepository = new Mock<IProductRepository>();
             mockProductRepository
                 .Setup(repo => repo.GetProductsAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<int>()))
-                .ReturnsAsync(sampleProducts);
+                .ReturnsAsync(new[] { _sampleProduct });
 
             var productService = new ProductService(mockProductRepository.Object, new Mock<IRepository<Category>>().Object);
 
@@ -80,7 +63,7 @@ namespace CatalogService.Tests.Services
             // Assert
             Assert.NotEmpty(result);
             Assert.True(result.Any());
-            Assert.Equal("Laptop ABC", result.ElementAt(0).Name);
+            Assert.Equal(_sampleProduct.Name, result.ElementAt(0).Name);
             mockProductRepository.Verify(x => x.GetProductsAsync(It.IsAny<int?>(), It.IsAny<int>(), It.IsAny<int>()), Times.Once);
         }
     }
